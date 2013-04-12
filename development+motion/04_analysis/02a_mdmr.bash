cdfc2083954bf1ca29d7f353de1ae146d1721467
Zarrar Shehzad <czarrar@gmail.com>
2013-04-12 15:22:48 -0400

    Cluster correction for power/dev+motion

--- development+motion/04_analysis/02a_mdmr.bash
+++ development+motion/04_analysis/02a_mdmr.bash
@@ -4,29 +4,7 @@ basedir="/home2/data/Projects/CWAS"
 indir="${basedir}/share/development+motion"
 outdir="${basedir}/development+motion"
 
-#connectir_mdmr.R -i ${outdir}/cwas/rois_random_k3200 \
-#    --formula "sex + tr + age" \
-#    --model ${indir}/subinfo/02_details.csv \
-#    --factors2perm "age" \
-#    --permutations 14999 \
-#    --forks 1 --threads 10 \
-#    --memlimit 12 \
-#    --save-perms \
-#    --ignoreprocerror \
-#    age_sex+tr.mdmr
-#
-#connectir_mdmr.R -i ${outdir}/cwas/rois_random_k3200 \
-#    --formula "sex + tr + mean_FD + age" \
-#    --model ${indir}/subinfo/02_details.csv \
-#    --factors2perm "age,mean_FD" \
-#    --permutations 14999 \
-#    --forks 1 --threads 10 \
-#    --memlimit 12 \
-#    --save-perms \
-#    --ignoreprocerror \
-#    age+motion_sex+tr.mdmr
-
-connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
+connectir_mdmr.R -i ${outdir}/cwas/rois_random_k3200 \
     --formula "sex + tr + age" \
     --model ${indir}/subinfo/02_details.csv \
     --factors2perm "age" \
@@ -37,7 +15,7 @@ connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
     --ignoreprocerror \
     age_sex+tr.mdmr
 
-connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
+connectir_mdmr.R -i ${outdir}/cwas/rois_random_k3200 \
     --formula "sex + tr + mean_FD + age" \
     --model ${indir}/subinfo/02_details.csv \
     --factors2perm "age,mean_FD" \
@@ -47,3 +25,28 @@ connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
     --save-perms \
     --ignoreprocerror \
     age+motion_sex+tr.mdmr
+
+
+# Not going to use the cwas_regress_motion dealio below
+
+#connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
+#    --formula "sex + tr + age" \
+#    --model ${indir}/subinfo/02_details.csv \
+#    --factors2perm "age" \
+#    --permutations 14999 \
+#    --forks 1 --threads 10 \
+#    --memlimit 12 \
+#    --save-perms \
+#    --ignoreprocerror \
+#    age_sex+tr.mdmr
+#
+#connectir_mdmr.R -i ${outdir}/cwas_regress_motion/rois_random_k3200 \
+#    --formula "sex + tr + mean_FD + age" \
+#    --model ${indir}/subinfo/02_details.csv \
+#    --factors2perm "age,mean_FD" \
+#    --permutations 14999 \
+#    --forks 1 --threads 10 \
+#    --memlimit 12 \
+#    --save-perms \
+#    --ignoreprocerror \
+#    age+motion_sex+tr.mdmr
