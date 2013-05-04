187a9bbabe1d6e4d8c9165e403f28141699c6feb
Zarrar Shehzad <czarrar@gmail.com>
2013-05-04 14:49:21 -0400

    subsample 2mm => 4mm

--- nki/03_post-process/20_subsample_4mm.R
+++ nki/03_post-process/20_subsample_4mm.R
@@ -44,7 +44,7 @@ for (si in 1:length(scans)) {
   # outfiles <- file.path(dirname(infiles), "functional_mni_4mm.nii.gz")
   
   for (infile in infiles) {
-    vcat(T, "...%s", infile)
+    #vcat(T, "...%s", infile)
     outfile <- file.path(dirname(infile), "functional_mni_4mm.nii.gz")
     if (file.exists(outfile))
       file.remove(outfile)
@@ -57,36 +57,36 @@ for (si in 1:length(scans)) {
 
 
 
-###
-# Below is the regular version (GLOBAL)
-###
-
-strategy <- "global"
-vcat(T, strategy)
-
-for (si in 1:length(scans)) {
-  # 1. input functional paths
-  flist <- file.path(subinfo, scan_folder[si], sprintf("%s_%s_funcpaths.txt", scans[si], strategy))
-  raw <- read.table(flist)[,1]
-  infiles <- as.character(raw)
-  infiles <- sub("/home/", "/home2/", infiles)
-  
-  # 2. 4mm resolution brain (i.e., the master)
-  stdfile <- "/home2/data/PublicProgram/fsl-4.1.9/data/standard/MNI152_T1_4mm_brain.nii.gz"
-  
-  # 3. command
-  cmd <- "3dresample -inset %s -master %s -rmode Linear -prefix %s"
-  
-  # outfiles <- file.path(dirname(infiles), "functional_mni_4mm.nii.gz")
-  
-  for (infile in infiles) {
-    vcat(T, "...%s", infile)
-    outfile <- file.path(dirname(infile), "functional_mni_4mm.nii.gz")
-    if (file.exists(outfile))
-      file.remove(outfile)
-    real_cmd <- sprintf(cmd, infile, stdfile, outfile)
-    
-    cat(real_cmd, "\n")
-    system(real_cmd)
-  }
-}
+####
+## Below is the regular version (GLOBAL)
+####
+#
+#strategy <- "global"
+#vcat(T, strategy)
+#
+#for (si in 1:length(scans)) {
+#  # 1. input functional paths
+#  flist <- file.path(subinfo, scan_folder[si], sprintf("%s_%s_funcpaths.txt", scans[si], strategy))
+#  raw <- read.table(flist)[,1]
+#  infiles <- as.character(raw)
+#  infiles <- sub("/home/", "/home2/", infiles)
+#  
+#  # 2. 4mm resolution brain (i.e., the master)
+#  stdfile <- "/home2/data/PublicProgram/fsl-4.1.9/data/standard/MNI152_T1_4mm_brain.nii.gz"
+#  
+#  # 3. command
+#  cmd <- "3dresample -inset %s -master %s -rmode Linear -prefix %s"
+#  
+#  # outfiles <- file.path(dirname(infiles), "functional_mni_4mm.nii.gz")
+#  
+#  for (infile in infiles) {
+#    #vcat(T, "...%s", infile)
+#    outfile <- file.path(dirname(infile), "functional_mni_4mm.nii.gz")
+#    if (file.exists(outfile))
+#      file.remove(outfile)
+#    real_cmd <- sprintf(cmd, infile, stdfile, outfile)
+#    
+#    cat(real_cmd, "\n")
+#    system(real_cmd)
+#  }
+#}
